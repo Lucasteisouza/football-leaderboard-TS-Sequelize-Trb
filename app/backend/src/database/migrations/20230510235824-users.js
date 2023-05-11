e4055d8858a1520abe09385477ea39b32b6e2c19
Lucasteisouza <lucasteisouza@gmail.com>
2023-05-10 21:27:24 -0300

    feat: efetiva as configurações iniciais do docker e as migrations

---
+++ app/backend/src/database/migrations/20230510235824-users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+        unique: true,
+      },
+      role: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
