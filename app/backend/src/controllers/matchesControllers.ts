930c00b96323dc8dea50b90d14950feeea5cdd20
Lucasteisouza <lucasteisouza@gmail.com>
2023-05-21 23:54:02 -0300

    fix: corrige a saida de status de um req de matches

--- app/backend/src/controllers/matchesControllers.ts
+++ app/backend/src/controllers/matchesControllers.ts
@@ -14,7 +14,7 @@ class MatchesControllers {
       boolInprogress = false;
     }
     const matches = await MatchesServices.getAllMatches(boolInprogress);
-    res.status(200).json(matches);
+    return res.status(200).json(matches);
   }
 
   public static async finishMatch(req: Request, res: Response) {
@@ -22,7 +22,7 @@ class MatchesControllers {
     const finishedMatch = await MatchesServices.finishMatch(
       parseInt(id, 10),
     );
-    res.status(200).json({ message: finishedMatch });
+    return res.status(200).json({ message: finishedMatch });
   }
 
   public static async updateScore(req: Request, res: Response) {
@@ -37,7 +37,7 @@ class MatchesControllers {
       homeTeamGoals,
       awayTeamGoals,
     );
-    res.status(200).json(updatedMatch);
+    return res.status(200).json(updatedMatch);
   }
 
   public static async createMatch(req: Request, res: Response) {
@@ -58,7 +58,7 @@ class MatchesControllers {
       homeTeamGoals,
       awayTeamGoals,
     );
-    res.status(201).json(match);
+    return res.status(201).json(match);
   }
 }
 
--- app/backend/src/tests/matchTests.test.ts
+++ app/backend/src/tests/matchTests.test.ts
@@ -1,75 +1,101 @@
-// import * as sinon from 'sinon';
-// import * as chai from 'chai';
-// import chaiHttp = require('chai-http');
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
 
-// import { app } from '../app';
-// import { Response } from 'superagent';
-// import MatchesModel from '../database/models/Matches';
+import { app } from '../app';
+import { Response } from 'superagent';
+import MatchesModel from '../database/models/Matches';
+import MatchesServices from '../services/matchesServices';
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// const { expect } = chai;
+const { expect } = chai;
 
-// const allMatchesMock = [
-//   {
-//     id: 1,
-//     homeTeamId: 16,
-//     homeTeamGoals: 1,
-//     awayTeamId: 8,
-//     awayTeamGoals: 1,
-//     inProgress: 0,
-//   },
-//   {
-//     id: 2,
-//     homeTeamId: 9,
-//     homeTeamGoals: 1,
-//     awayTeamId: 14,
-//     awayTeamGoals: 1,
-//     inProgress: 0,
-//   },
-//   {
-//     id: 3,
-//     homeTeamId: 4,
-//     homeTeamGoals: 3,
-//     awayTeamId: 11,
-//     awayTeamGoals: 0,
-//     inProgress: 1,
-//   },
-// ]
+const allMatchesMock = [
+  {
+    id: 1,
+    homeTeamId: 16,
+    homeTeamGoals: 1,
+    awayTeamId: 8,
+    awayTeamGoals: 1,
+    inProgress: 0,
+  },
+  {
+    id: 2,
+    homeTeamId: 9,
+    homeTeamGoals: 1,
+    awayTeamId: 14,
+    awayTeamGoals: 1,
+    inProgress: 0,
+  },
+  {
+    id: 3,
+    homeTeamId: 4,
+    homeTeamGoals: 3,
+    awayTeamId: 11,
+    awayTeamGoals: 0,
+    inProgress: 1,
+  },
+]
 
-// describe ('Matches tests', () => {
-//   afterEach(() => {
-//     sinon.restore();
-//   });
+const allMatchesMockNoId = [
+  {
+    homeTeamId: 16,
+    homeTeamGoals: 1,
+    awayTeamId: 8,
+    awayTeamGoals: 1,
+    inProgress: 0,
+  },
+  {
+    homeTeamId: 9,
+    homeTeamGoals: 1,
+    awayTeamId: 14,
+    awayTeamGoals: 1,
+    inProgress: 0,
+  },
+  {
+    homeTeamId: 4,
+    homeTeamGoals: 3,
+    awayTeamId: 11,
+    awayTeamGoals: 0,
+    inProgress: 1,
+  },
+]
 
-//   let chaiHttpResponse: Response;
+describe ('Matches tests', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  let chaiHttpResponse: Response;
 
 
-//   it('should return a list with all matches' , async () => {
-//     sinon.stub(MatchesModel, 'findAll').resolves(allMatchesMock as unknown as MatchesModel[]);
-//     chaiHttpResponse = await chai.request(app).get('/matches');
-//     expect(chaiHttpResponse.status).to.be.equal(200);
-//     expect(chaiHttpResponse.body).to.be.deep.equal(allMatchesMock);
-//   });
+  it('should return a list with all matches' , async () => {
+    sinon.stub(MatchesServices, 'getAllMatches').resolves(allMatchesMock as unknown as MatchesModel[]);
+    chaiHttpResponse = await chai.request(app).get('/matches');
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.be.deep.equal(allMatchesMock);
+  });
 
-//   it('should return in progress matches only', async () => {
-//     sinon.stub(MatchesModel, 'findAll').resolves(allMatchesMock as unknown as MatchesModel[]);
-//     chaiHttpResponse = await chai.request(app).get('/matches/?inProgress=true');
-//     expect(chaiHttpResponse.status).to.be.equal(200);
-//     expect(chaiHttpResponse.body).to.be.deep.equal([allMatchesMock[2]]);
-//   });
+  // it('should return in progress matches only', async () => {
+  //   sinon.stub(MatchesServices, 'getAllMatches').resolves(allMatchesMock as unknown as MatchesModel[]);
+  //   chaiHttpResponse = await chai.request(app).get('/matches/?inProgress=true');
+  //   expect(chaiHttpResponse.status).to.be.equal(200);
+  //   expect(chaiHttpResponse.body).to.be.deep.equal([allMatchesMock[2]]);
+  // });
 
-//   it('should return in NOT progress matches only', async () => {
-//     sinon.stub(MatchesModel, 'findAll').resolves(allMatchesMock as unknown as MatchesModel[]);
-//     chaiHttpResponse = await chai.request(app).get('/matches/?inProgress=false');
-//     expect(chaiHttpResponse.status).to.be.equal(200);
-//     expect(chaiHttpResponse.body).to.be.deep.equal([allMatchesMock[0], allMatchesMock[1]]);
-//   });
+  // it('should return in NOT progress matches only', async () => {
+  //   sinon.stub(MatchesServices, 'getAllMatches').resolves(allMatchesMock as unknown as MatchesModel[]);
+  //   chaiHttpResponse = await chai.request(app).get('/matches/?inProgress=false');
+  //   expect(chaiHttpResponse.status).to.be.equal(200);
+  //   expect(chaiHttpResponse.body).to.be.deep.equal([allMatchesMock[0], allMatchesMock[1]]);
+  // });
 
-//   it('should finish a game and return "finished"', async () => {
-//     sinon.stub(MatchesModel, 'findByPk'). resolves(allMatchesMock[0] as unknown as MatchesModel);
-//     chaiHttpResponse = await chai.request(app).get('/1/finish');
-//     expect(chaiHttpResponse.status).to.be.equal(200);
-//     expect(chaiHttpResponse.body.message).to.be.equal('Finished')
-//   });
-// });
+  // it('should finish a game and return "finished"', async () => {
+  //   sinon.stub(MatchesServices, 'finishMatch'). resolves('Finished');
+  //   chaiHttpResponse = await chai.request(app).patch('/1/finish');
+  //   expect(chaiHttpResponse.status).to.be.equal(200);
+  //   expect(chaiHttpResponse.body.message).to.be.equal({ message: 'Finished' });
+  // });
+});
